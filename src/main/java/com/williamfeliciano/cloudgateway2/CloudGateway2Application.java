2a84e080789a57913e21ae44c843edfeec06080c
William A Feliciano Rios <31602072+wfeliciano20@users.noreply.github.com>
2023-04-19 23:25:34 -0400

    Initial Commit

---
+++ src/main/java/com/williamfeliciano/cloudgateway2/CloudGateway2Application.java
@@ -0,0 +1,13 @@
+package com.williamfeliciano.cloudgateway2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudGateway2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudGateway2Application.class, args);
+    }
+
+}
