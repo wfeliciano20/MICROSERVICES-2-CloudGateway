bc6aac3e4247a082ae1d42116d24f8d905a552dc
William A Feliciano Rios <31602072+wfeliciano20@users.noreply.github.com>
2023-04-20 22:08:49 -0400

    renamed application.java

--- src/main/java/com/williamfeliciano/cloudgateway2/CloudGatewayApplication.java
+++ src/main/java/com/williamfeliciano/cloudgateway2/CloudGatewayApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CloudGateway2Application {
+public class CloudGatewayApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(CloudGateway2Application.class, args);
+        SpringApplication.run(CloudGatewayApplication.class, args);
     }
 
 }
