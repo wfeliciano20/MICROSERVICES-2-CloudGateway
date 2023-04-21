15c2637106d5e4e4ba124d082ca07dbc14d9ea4e
William A Feliciano Rios <31602072+wfeliciano20@users.noreply.github.com>
2023-04-20 23:03:43 -0400

    Implemented CircuitBreaker

---
+++ src/main/java/com/williamfeliciano/cloudgateway2/controller/FallbackController.java
@@ -0,0 +1,23 @@
+package com.williamfeliciano.cloudgateway2.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FallbackController {
+
+    @GetMapping("/orderServiceFallBack")
+    public String orderServiceFallback(){
+        return "Order Service is Down";
+    }
+
+    @GetMapping("/productServiceFallBack")
+    public String productServiceFallBack(){
+        return "Product Service is Down";
+    }
+
+    @GetMapping("/paymentServiceFallBack")
+    public String paymentServiceFallBack(){
+        return "Payment Service is Down";
+    }
+}
