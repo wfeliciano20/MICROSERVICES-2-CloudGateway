9cb80dc3766a5e63199836b941a7f6dac25140cc
William A Feliciano Rios <31602072+wfeliciano20@users.noreply.github.com>
2023-04-20 23:48:51 -0400

    Implemented Rate Limiter with redis

--- src/main/java/com/williamfeliciano/cloudgateway2/CloudGatewayApplication.java
+++ src/main/java/com/williamfeliciano/cloudgateway2/CloudGatewayApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JCircuitBreakerFactory;
 import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JConfigBuilder;
 import org.springframework.cloud.client.circuitbreaker.Customizer;
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
 import org.springframework.context.annotation.Bean;
+import reactor.core.publisher.Mono;
 
 
 @SpringBootApplication
@@ -15,6 +17,10 @@ public class CloudGatewayApplication {
 
     public static void main(String[] args) { SpringApplication.run(CloudGatewayApplication.class, args); }
 
+    @Bean
+    KeyResolver userKeyResolver(){
+        return exchange -> Mono.just("userKey");
+    }
     @Bean
     public Customizer<Resilience4JCircuitBreakerFactory> defaultCustomizer(){
         return factory -> factory.configureDefault(
